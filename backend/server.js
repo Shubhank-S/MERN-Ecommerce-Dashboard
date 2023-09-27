d4645a9635863675788c337c88a38537c2e06187
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-27 16:13:15 +0530

    Database Connectivity Done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import UserRoutes from './routes/UserRoutes.js'
+import connectDB from './config/db.js';
 
-dotenv.config();
 const app = express();
 
 // .env Configuration
+
+dotenv.config();
+
+connectDB();
 
 // PORT
 
@@ -14,6 +18,7 @@ const PORT = process.env.PORT;
 // Routes
 
 app.use('/api/v1/user',UserRoutes)
+
 
 app.listen(PORT,()=>{
   console.log(`Server Running at Port ${PORT}`)
