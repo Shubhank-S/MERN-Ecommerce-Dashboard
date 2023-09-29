859638f61c0d2d3423ba6d1bdfaf8fbe0fd13711
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-29 18:31:43 +0530

    Get-Single-Product API Created

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,20 +1,29 @@
 import ProductModel from "../models/ProductModel.js";
 
-export const addProduct= async (req,resp)=>{
-  try {
-    const product = new ProductModel(req.body)
-    const productSave = await product.save();
-    resp.send(productSave)
-  } catch (error) {
-    console.log(error)
-  }
+export const addProduct = async (req, resp) => {
+    try {
+        const product = new ProductModel(req.body)
+        const productSave = await product.save();
+        resp.send(productSave)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const getProduct= async(req,resp)=>{
-try {
-    const findProduct = await ProductModel.find({})
-    resp.send(findProduct)
-} catch (error) {
-    console.log(error)
+export const getProduct = async (req, resp) => {
+    try {
+        const findProduct = await ProductModel.find({})
+        resp.send(findProduct)
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+export const getSingleProduct = async (req, resp) => {
+    try {
+        const productDetail = await ProductModel.findById({ _id: req.params.id })
+        resp.send(productDetail)
+    } catch (error) {
+        console.log(error)
+    }
 }
--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addProduct, getProduct } from '../controllers/ProductController.js';
+import { addProduct, getProduct, getSingleProduct } from '../controllers/ProductController.js';
 
 
 const router = express.Router()
@@ -11,5 +11,9 @@ router.post('/add-product',addProduct)
 // Get Product from DB || GET
 
 router.get('/get-product',getProduct)
+
+// Get Single Product from DB || GET
+
+router.get('/get-product/:id',getSingleProduct)
 
 export default router;
