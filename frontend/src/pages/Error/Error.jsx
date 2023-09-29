bdec056a88a847b87558f8e2a91f8e610a51be1f
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-30 01:09:45 +0530

    Worked on Register Page

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -5,8 +5,8 @@ function Error() {
   const navigate = useNavigate();
   return (
     <div className="error">
-      <h1>404</h1>
-      <h1>Not Found</h1>
+      <h1 className="error_h1"> 404</h1>
+      <h1 className="error_h2">Not Found</h1>
       <button onClick={() => navigate("/")}>Go Back</button>
     </div>
   );
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./Register.css";
 
 function Register() {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name...."
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email...."
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password...."
+        />
+        <button>Register</button>
+      </form>
+    </>
+  );
 }
 
 export default Register;
