36b30fda42204b31cf2a45f3270c35e4014dbf5a
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-29 11:14:08 +0530

    Add-Product API Created

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,5 +1,11 @@
 import ProductModel from "../models/ProductModel.js";
 
-export const addProduct=(req,resp)=>{
-
+export const addProduct= async (req,resp)=>{
+  try {
+    const product = new ProductModel(req.body)
+    const productSave = await product.save();
+    resp.send(productSave)
+  } catch (error) {
+    console.log(error)
+  }
 }
--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -4,6 +4,8 @@ import { addProduct } from '../controllers/ProductController.js';
 
 const router = express.Router()
 
+// Add Product || POST
+
 router.post('/add-product',addProduct)
 
 export default router;
