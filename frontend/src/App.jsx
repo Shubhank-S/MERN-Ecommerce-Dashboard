988b2a0bafad2a69e628f6e4b1488430c83a093a
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-29 23:31:01 +0530

    Worked On the Header Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element />
+          <Route path="/" element />
+          <Route path="/" element />
+          <Route path="/" element />
+          <Route path="/" element />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
 export default App;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <ul className="header_links">
+        <li>
+          <NavLink className="links">Register</NavLink>
+        </li>
+        <li>
+          <NavLink className="links">LOGIN</NavLink>
+        </li>
+        <li>
+          <NavLink className="links">Add-Products</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
