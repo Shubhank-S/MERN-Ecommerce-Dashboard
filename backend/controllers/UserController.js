34275062e3a4843d2275d7c096013774e6624e7a
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-30 16:46:12 +0530

    Toast Notifcation Added and Register part done

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,20 +1,20 @@
 import UserModel from "../models/UserModel.js"
 
-export const registerController = async  (req,resp)=>{
+export const registerController = async (req, resp) => {
   try {
     const user = new UserModel(req.body)
     const userSave = await user.save()
     resp.send(userSave)
   } catch (error) {
     console.log(`Error is ${error}`)
-}
+  }
 }
 
-export const loginController= async(req,resp)=>{
-    try {
-        const user = await UserModel.find({email:req.body.email})
-        resp.send(user)
-    } catch (error) {
+export const loginController = async (req, resp) => {
+  try {
+    const user = await UserModel.find({ email: req.body.email })
+    resp.send(user)
+  } catch (error) {
     console.log(`Error is ${error}`)
   }
 }
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const existedUser = localStorage.getItem("user");
+    if (existedUser) {
+      navigate("/");
+      toast.error("User Already Exist");
+    }
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,34 +36,44 @@ function Register() {
       );
       console.log(result.data);
       localStorage.setItem("user", JSON.stringify(result.data));
+      navigate("/");
+      toast.success("Registered SuccessFully");
     } catch (error) {
       console.log(`Error is ${error}`);
     }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <h1>Register</h1>
+      <form onSubmit={handleSubmit} className="register_form">
+        <h2>Register</h2>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Enter your name...."
+          className="register_input"
         />
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter your email...."
+          className="register_input"
         />
         <input
           type="text"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter your password...."
+          className="register_input"
         />
-        <button onClick={RegisterData}>Register</button>
+        <button onClick={RegisterData} className="register_button">
+          Register
+        </button>
       </form>
+      <div>
+        <Toaster position="top-center" reverseOrder={false} />
+      </div>
     </>
   );
 }
